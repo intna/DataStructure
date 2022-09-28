eddd60f89d34178d4c0f1cba1b114692fe2399e6
int <intna@123.com>
2022-09-28 22:43:10 +0800

    ok

--- DataStructure/src/com/data/hua/linkedList/Josephu.java
+++ DataStructure/src/com/data/hua/linkedList/Josephu.java
@@ -1,6 +1,5 @@
 package com.data.hua.linkedList;
 
-import java.util.Properties;
 
 /**
  * 约瑟夫问题
@@ -39,18 +38,54 @@ class CricleSingleLinkedList{
     //遍历当前环形链表
     public void showBoy() {
        if(first == null){
-        System.out.println("环形链表为空");
-        return;
+          System.out.println("环形链表为空");
+          return;
        }
        Boy curBoy = first;
        while(true){
-        System.out.println("小孩的编号是" + curBoy.getNo());
-        if(curBoy.getNext() == first){
-           break;
+          System.out.println("小孩的编号是" + curBoy.getNo());
+          if(curBoy.getNext() == first){
+             break;
         }
         curBoy = curBoy.getNext();
        }
-       
+    }
+    //解决约瑟夫问题:startNO:表示从第几个小孩开始数。countNum:表示数几下。nums:表示最初有多少个小孩在圈中
+    public void conntBoy(int startNO,int countNum,int nums){
+       //先进行数据校验 
+       if (first == null || startNO < 1 || startNO > nums) {
+          System.out.println("参数输入有误，请重新输入");
+       }
+      //创建辅助指针，帮助小孩出圈
+        Boy helper = first;
+        //helper最先应指向环形链表的最后这个结点
+        while(true){
+           if(helper.getNext() == first){
+               break;
+           }
+           helper = helper.getNext();
+        }  
+       //小孩报数前，先让helper和first移动staetNo-1次
+       for (int j = 0; j < startNO; j++) {
+           first = first.getNext();
+           helper = helper.getNext();
+        
+       }
+       //小孩报数
+       while(true){
+           if(helper.getNext() == first){
+            break;
+           }
+            for(int j = 0; j < countNum-1;j++){
+                first = first.getNext();
+                helper = helper.getNext();
+            } 
+            //此时first指向的小孩就是要出圈的小孩
+            first = first.getNext();
+            helper.setNext(first);       
+        }
+       }
+
 
     }
 }
